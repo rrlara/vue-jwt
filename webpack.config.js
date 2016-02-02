a1caba349cda60d8c9d4f14b52a4073da512016c
Rene Rodriguez <rrlara@gmail.com>
2016-02-01 17:41:14 -0800

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+var webpack = require('webpack')
+
 module.exports = {
   // the main entry of our app
   entry: ['./src/index.js', './src/auth/index.js'],
   // output configuration
   output: {
-    path: __dirname + '/build/',
-    publicPath: 'build/',
+    path: '/build',
+    publicPath: '/build/',
     filename: 'build.js'
   },
   // how modules should be transformed
@@ -24,4 +26,22 @@ module.exports = {
     presets: ['es2015'],
     plugins: ['transform-runtime']
   }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
 }
